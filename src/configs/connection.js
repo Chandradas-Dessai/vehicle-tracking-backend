66ab4b1f4853983ca07bd0e1611b4cf897233eb0
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 14:00:20 +0530

    Update connection.js

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -19,7 +19,9 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
     host: env.host,
     dialect: env.dialect,
-    operatorsAliases: false,
+    "dialectOptions": {
+          "ssl": true
+        },
    
     pool: {
       max: env.max,
@@ -28,5 +30,5 @@ const sequelize = new Sequelize(env.database, env.username, env.password, {
       idle: env.pool.idle
     }
   });
-  
+
 module.exports = sequelize;
