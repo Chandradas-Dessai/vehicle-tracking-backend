4046ae2165f38f287aa38479ddad0cd93390ecf5
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 13:38:46 +0530

    Update connection.js

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -1,8 +1,25 @@
 const env = require('./env');
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.db_name, env.db_username, env.db_password, {
-    host: env.db_host,
+// const sequelize = new Sequelize(env.db_name, env.db_username, env.db_password, {
+//     host: env.db_host,
+//     dialect: env.db_dialect,
+//     define: {
+//         charset: 'utf8',
+//         collate: 'utf8_general_ci',
+//         timestamps: true
+//     },
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// });
+
+const sequelize = new Sequelize(process.env.db_url, {
     dialect: env.db_dialect,
+    protocol: env.db_protocol,
+    dialectOptions: env.db_dialectOptions,
     define: {
         charset: 'utf8',
         collate: 'utf8_general_ci',
@@ -15,4 +32,5 @@ const sequelize = new Sequelize(env.db_name, env.db_username, env.db_password, {
         idle: 10000
     }
 });
+module.exports = sequelize;
 module.exports = sequelize;
