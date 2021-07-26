44fab5e02f235b93de5708111c16ec83fb8c395c
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 13:46:32 +0530

    Update env.js

--- src/configs/env.js
+++ src/configs/env.js
@@ -8,7 +8,8 @@ module.exports = {
     "db_password":"root",
     "db_host":"localhost",
     "db_dialect":"postgres",
-    "use_env_variable": "DATABASE_URL",
+    "db_protocol": "postgres",
+    "db_url": "ibgvqlpvxmzafz:648ee74e3884d842b425724ccfc335a94cadafb7e366bd7164b8246327da0be9@ec2-34-228-100-83.compute-1.amazonaws.com:5432/d4a11q4ifmusoi",
 "dialectOptions": {
     "ssl": true
   }
