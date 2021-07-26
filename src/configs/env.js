45ca9b2c37dd5c610ec75a54662bb39e1148c73e
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 13:15:24 +0530

    Update env.js

--- src/configs/env.js
+++ src/configs/env.js
@@ -7,7 +7,10 @@
 //     "db_username":"postgres",
 //     "db_password":"root",
 //     "db_host":"localhost",
-//     "db_dialect":"postgres"
+//     "db_dialect":"postgres",
+// "dialectOptions": {
+//     "ssl": true
+//   }
 // };
 
 
@@ -20,5 +23,8 @@ module.exports = {
     "db_username":"ibgvqlpvxmzafz",
     "db_password":"648ee74e3884d842b425724ccfc335a94cadafb7e366bd7164b8246327da0be9",
     "db_host":"ec2-34-228-100-83.compute-1.amazonaws.com",
-    "db_dialect":"postgres"
+    "db_dialect":"postgres",
+    "dialectOptions": {
+        "ssl": true
+      }
 };
