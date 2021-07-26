e94e54b2744f39b0f42f00731e825a46c4205da4
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 20:58:31 +0530

    added the db config

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -1,34 +1,26 @@
 const env = require('./env');
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(env.db_name, env.db_username, env.db_password, {
-//     host: env.db_host,
-//     dialect: env.db_dialect,
-//     define: {
-//         charset: 'utf8',
-//         collate: 'utf8_general_ci',
-//         timestamps: true
-//     },
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// });
 
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-    host: env.host,
-    dialect: env.dialect,
-    "dialectOptions": {
-          "ssl": true
-        },
-   
+const sequelize = new Sequelize(env.db_name, env.db_username, env.db_password, {
+    host: env.db_host,
+    dialect: env.db_dialect,
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false 
+        }
+      },
+    define: {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        timestamps: true
+    },
     pool: {
-      max: env.max,
-      min: env.pool.min,
-      acquire: env.pool.acquire,
-      idle: env.pool.idle
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
-  });
+});
 
 module.exports = sequelize;
