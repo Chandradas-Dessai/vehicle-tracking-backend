294ea59868ea13da814fd8a44cb6ef89564a0203
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 21:10:16 +0530

    Update env.js

--- src/configs/env.js
+++ src/configs/env.js
@@ -14,12 +14,15 @@
 // };
 
 module.exports = {
+    "port": 3000,
+    "appEndpoint": "http://localhost:3000",
+    "apiEndpoint": "http://localhost:3000",
+    "environment": "dev",
     "db_name":"d4a11q4ifmusoi",
     "db_username":"ibgvqlpvxmzafz",
     "db_password":"648ee74e3884d842b425724ccfc335a94cadafb7e366bd7164b8246327da0be9",
     "db_host":"ec2-34-228-100-83.compute-1.amazonaws.com",
     "db_dialect":"postgres",
-    "port": 5432,
     "dialectOptions": {
     "ssl": true
     }
