2fe114536aefdac5fb822b9183cfee22ac6e0610
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 23:06:58 +0530

    Update env.js

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,23 +1,23 @@
-module.exports = {
-    "port": 4000,
-    "appEndpoint": "http://localhost:4000",
-    "apiEndpoint": "http://localhost:4000",
-    "environment": "dev",
-    "db_name":"vehicle-app",
-    "db_username":"postgres",
-    "db_password":"root",
-    "db_host":"localhost",
-    "db_dialect":"postgres",
-};
-
 // module.exports = {
-//     "appEndpoint": process.env.APP_ENDPOINT,
-//     "apiEndpoint": process.env.API_ENDPOINT,
-//     "environment": process.env.ENVIRONMENT,
-//     "db_name": process.env.DB_DATABASE,
-//     "db_username": process.env.DB_USERNAME,
-//     "db_password":process.env.DB_PASSWORD,
-//     "db_host": process.env.DB_HOST,
-//     "port": process.env.DB_PORT,
-//     "db_dialect": process.env.DB_DIALECT
+//     "port": 4000,
+//     "appEndpoint": "http://localhost:4000",
+//     "apiEndpoint": "http://localhost:4000",
+//     "environment": "dev",
+//     "db_name":"vehicle-app",
+//     "db_username":"postgres",
+//     "db_password":"root",
+//     "db_host":"localhost",
+//     "db_dialect":"postgres",
 // };
+
+module.exports = {
+    "port": process.env.DB_PORT,
+    "appEndpoint": process.env.APP_ENDPOINT,
+    "apiEndpoint": process.env.API_ENDPOINT,
+    "environment": process.env.ENVIRONMENT,
+    "db_name": process.env.DB_DATABASE,
+    "db_username": process.env.DB_USERNAME,
+    "db_password":process.env.DB_PASSWORD,
+    "db_host": process.env.DB_HOST,
+    "db_dialect": process.env.DB_DIALECT
+};
