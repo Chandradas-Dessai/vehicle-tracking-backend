42a9fc88645a7d7a28e9df68db35f56535656368
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2021-07-26 13:10:26 +0530

    Update env.js

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,24 +1,24 @@
-module.exports = {
-    "port": 4000,
-    "appEndpoint": "http://localhost:4000",
-    "apiEndpoint": "http://localhost:4000",
-    "environment": "dev",
-    "db_name":"vehicle-app",
-    "db_username":"postgres",
-    "db_password":"root",
-    "db_host":"localhost",
-    "db_dialect":"postgres"
-};
-
-
 // module.exports = {
 //     "port": 4000,
-//     "appEndpoint": "https://vehicle-node-backend.herokuapp.com:4000",
-//     "apiEndpoint": "https://vehicle-node-backend.herokuapp.com:4000",
+//     "appEndpoint": "http://localhost:4000",
+//     "apiEndpoint": "http://localhost:4000",
 //     "environment": "dev",
-//     "db_name":"d4a11q4ifmusoi",
-//     "db_username":"ibgvqlpvxmzafz",
-//     "db_password":"648ee74e3884d842b425724ccfc335a94cadafb7e366bd7164b8246327da0be9",
-//     "db_host":"ec2-34-228-100-83.compute-1.amazonaws.com",
+//     "db_name":"vehicle-app",
+//     "db_username":"postgres",
+//     "db_password":"root",
+//     "db_host":"localhost",
 //     "db_dialect":"postgres"
 // };
+
+
+module.exports = {
+    "port": 4000,
+    "appEndpoint": "https://vehicle-node-backend.herokuapp.com:4000",
+    "apiEndpoint": "https://vehicle-node-backend.herokuapp.com:4000",
+    "environment": "dev",
+    "db_name":"d4a11q4ifmusoi",
+    "db_username":"ibgvqlpvxmzafz",
+    "db_password":"648ee74e3884d842b425724ccfc335a94cadafb7e366bd7164b8246327da0be9",
+    "db_host":"ec2-34-228-100-83.compute-1.amazonaws.com",
+    "db_dialect":"postgres"
+};
